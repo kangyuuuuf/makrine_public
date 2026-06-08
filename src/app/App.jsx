541f27baaba88f6ebdaee95106040d38c20c6452
Kangyu Feng <ericfeng0821@gmail.com>
2026-06-08 10:31:10 -0500

    add Analytics

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import ScrollToTop from './ScrollToTop.jsx'
+import { Analytics } from '@vercel/analytics/next';
 import Navbar from '../components/Navbar/Navbar.jsx'
 import HomePage from '../pages/Home/index.jsx'
 import AboutPage from '../pages/About/AboutPage.jsx'
@@ -14,6 +15,7 @@ function App() {
   return (
     <BrowserRouter basename={basename}>
       <ScrollToTop />
+      <Analytics />
       <div className="site-layout">
         <Navbar />
         <Routes>
